2d96abf112714b742492e834ad4c279861e09212
Fabiano Nascimento <fabianorodrigo@gmail.com>
2019-04-27 15:49:56 -0300

    Gravação do arquivo de dados local

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const fs = require('fs');
 const colors = require('colors');
 const bunyan = require('bunyan');
 const RotatingFileStream = require('bunyan-rotating-file-stream');
+const readlineSync = require('readline-sync');
 const GitHubService = require('./GitHubService');
 
 const logOptions = {
@@ -48,59 +50,111 @@ console.log(lineGraph.yellow);
   let page = 0;
   const perPage = 100;
 
+  const filePath = './data/gitHubSolidityRepos.json';
+  let solidityRepos = {};
   let repos = null;
   const reposWithTest = [];
 
-  do {
-    page++;
-    // Retrieve all Solidity repositories
-    repos = await gitService.getReposByLanguage({
-      language: 'Solidity',
-      sort: 'stars',
-      order: 'desc',
-      page,
-      per_page: perPage,
-    });
-    console.log(repos.total_count, repos.items.length);
+  if (fs.existsSync(filePath)) {
+    solidityRepos = JSON.parse(fs.readFileSync(filePath, { encoding: 'UTF8' }));
+  }
 
-    // https://stackoverflow.com/questions/37576685/using-async-await-with-a-foreach-loop
-    for (const r of repos.items) {
-      // console.log(r);
-      console.log(colors.yellow(r.full_name), ':', r.description);
-      console.log('URL JSON:', r.url);
-      console.log('URL:', r.html_url);
-      console.log('GIT:', r.git_url);
-      console.log('Stars:', r.stargazers_count);
-      // Just to don't be interpreted as abuse detection by github.com
-      await new Promise(done => setTimeout(done, 2000));
-      // const searchResult = await gitService.getExpressionInContent({ repo: r.name, expression });
-      // const searchResult = await gitService.getFilesInRepo({ repo: r.full_name, name: 'test' });
-      const searchResult = await gitService.searchRepositoryWithDiretory({
-        repo: r.name,
-        owner: r.owner.login,
-        directoriesNames: ['test', 'tests'],
+  const functionality = readlineSync.question(`What do you want to do?
+  1. Retrieve/update local data from Github
+  2. Analyse local data
+
+  Your choice: `);
+
+  if (functionality === '2') {
+    const reposLoaded = Object.values(solidityRepos);
+    const repoWithTestsAndStars = reposLoaded.filter(
+      sr => sr.searchResults.length > 0 && sr.repo.stargazers_count > 0,
+    );
+    repoWithTestsAndStars.sort((a, b) => {
+      if (a.repo.stargazers_count < b.repo.stargazers_count) {
+        return 1;
+      }
+      if (a.repo.stargazers_count > b.repo.stargazers_count) {
+        return -1;
+      }
+      // a deve ser igual a b
+      return 0;
+    });
+    console.log(
+      `######################### STARED REPOS WITH TEST TREES: ${repoWithTestsAndStars.length} / ${
+        reposLoaded.length
+      } #########################`,
+    );
+    repoWithTestsAndStars.forEach((r) => {
+      console.log(colors.yellow(r.repo.full_name), colors.blue(r.repo.stargazers_count));
+      console.log('Tests Trees:', r.searchResults.length);
+      console.log(r.repo.git_url);
+    });
+  } else if (functionality === '1') {
+    do {
+      page++;
+      // Retrieve all Solidity repositories
+      repos = await gitService.getReposByLanguage({
+        language: 'Solidity',
+        sort: 'stars',
+        order: 'desc',
+        page,
+        per_page: perPage,
       });
-      console.log(searchResult);
-      if (searchResult.length > 0) {
-        reposWithTest.push({
-          name: r.name,
-          owner: r.owner.login,
-          cloneUrl: r.clone_url,
-          stars: r.stargazers_count,
-          r,
-        });
-      }
-      if (searchResult instanceof Error) {
-        // wait 10s and try again
-        await new Promise(done => setTimeout(done, 10000));
-        const searchResult = await gitService.searchRepositoryWithDiretory({
-          repo: r.name,
-          owner: r.owner.login,
-          directoriesNames: ['test', 'tests'],
-        });
-        console.log(searchResult);
-      }
-      /* if (searchResult.items && searchResult.items.length > 0) {
+      console.log(repos.total_count, repos.items.length);
+
+      // https://stackoverflow.com/questions/37576685/using-async-await-with-a-foreach-loop
+      for (const r of repos.items) {
+        console.log(colors.yellow(r.full_name), ':', r.description);
+        console.log('URL JSON:', r.url);
+        console.log('URL:', r.html_url);
+        console.log('GIT:', r.git_url);
+        console.log('Stars:', r.stargazers_count);
+        if (!solidityRepos[r.full_name]) {
+          solidityRepos[r.full_name] = {
+            repo: r,
+            searchResults: [],
+          };
+          console.log(colors.cyan('NEW'));
+        } else if (solidityRepos[r.full_name].repo.stargazers_count != r.stargazers_count) {
+          console.log(
+            colors.magenta(
+              `UPDATE STARS from ${solidityRepos[r.full_name].repo.stargazers_count} to ${
+                r.stargazers_count
+              }`,
+            ),
+          );
+          solidityRepos[r.full_name].repo.stargazers_count = r.stargazers_count;
+          fs.writeFileSync(filePath, solidityRepos, { encoding: 'UTF8' });
+        }
+
+        // search only if there is no item in the results
+        if (solidityRepos[r.full_name].searchResults.length == 0) {
+          // Just to don't be interpreted as abuse detection by github.com
+          await new Promise(done => setTimeout(done, 2000));
+          const searchResult = await gitService.searchRepositoryWithDiretory({
+            repo: r.name,
+            owner: r.owner.login,
+            directoriesNames: ['test', 'tests'],
+          });
+
+          if (searchResult instanceof Error) {
+            // wait 10s and try again
+            await new Promise(done => setTimeout(done, 10000));
+            const searchResult = await gitService.searchRepositoryWithDiretory({
+              repo: r.name,
+              owner: r.owner.login,
+              directoriesNames: ['test', 'tests'],
+            });
+            if (searchResult instanceof Error) {
+              console.log(r.owner.login, r.name, colors.red(e.message));
+            }
+          } else {
+            solidityRepos[r.full_name].searchResults = searchResult;
+          }
+          fs.writeFileSync(filePath, JSON.stringify(solidityRepos, null, 4), { encoding: 'UTF8' });
+        }
+        /* if (searchResult.items && searchResult.items.length > 0) {
             console.log(colors.blue(`${r.name} contains '${expression}'`));
             await Promise.all(searchResult.map(async sr => {
                 console.log(r.name, sr);
@@ -109,11 +163,12 @@ console.log(lineGraph.yellow);
         } else {
             console.log(colors.red(`${r.name} DOES NOT contain '${expression}'`));
         } */
-    }
-  } while (page * perPage < repos.total_count);
+      }
+    } while (page * perPage < repos.total_count);
 
-  console.log(colors.blue(`${reposWithTest.length} of ${repos.items.length} repos with tests`));
-  reposWithTest.forEach((p) => {
-    console.log(p.name);
-  });
+    console.log(colors.blue(`${reposWithTest.length} of ${repos.items.length} repos with tests`));
+    reposWithTest.forEach((p) => {
+      console.log(p.name);
+    });
+  }
 })();
